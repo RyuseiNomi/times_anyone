9ce959d4ee996d2374e9bc389d989a151b535966
RyuseiNomi <kanontonto1010@gmail.com>
2019-11-18 22:20:31 +0900

    Initial Commit

---
+++ times_someone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  times_someone
+//
+//  Created by 能美龍星 on 2019/11/18.
+//  Copyright © 2019 Ryusei Nomi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
