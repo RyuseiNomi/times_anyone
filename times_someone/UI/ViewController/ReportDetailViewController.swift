48f8c69e65027f5771e9949ce263cb161b367676
RyuseiNomi <kanontonto1010@gmail.com>
2019-11-23 15:05:01 +0900

    [WIP]Add post button on table view

--- times_someone/UI/ViewController/ReportDetailViewController.swift
+++ times_someone/UI/ViewController/ReportDetailViewController.swift
@@ -11,25 +11,13 @@ import UIKit
 class ReportDetailViewController: UIViewController {
 
     let titleLabel = UILabel()
-    let createReportButton = UIButton()
-    
-    let reportPostPresenter = ReportPostPresenter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
         
-        //titleLabel.text = "times_someone"
         titleLabel.textAlignment = .center
         view.addSubview(titleLabel)
-        
-        createReportButton.frame = CGRect(x: 0, y: 0, width: 150, height: 50)
-        createReportButton.layer.position = CGPoint(x: view.frame.width - view.frame.width/10*2, y:view.frame.height - view.frame.height/10*2)
-        createReportButton.setTitleColor(UIColor.white, for: UIControl.State.normal)
-        createReportButton.setTitle("投稿", for: UIControl.State.normal)
-        //TODO ターゲットをReportPostPresenterに向ける
-        createReportButton.addTarget(self, action: #selector(reportPostPresenter.postReport), for: .touchUpInside)
-        view.addSubview(createReportButton)
     }
 
     override func viewDidLayoutSubviews() {
