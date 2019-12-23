228fc622a776dc8d554a98edf7b2832de2122b44
RyuseiNomi <kanontonto1010@gmail.com>
2019-12-23 09:24:21 +0900

    add new class

---
+++ times_someone/UI/ViewController/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  times_someone
+//
+//  Created by 能美龍星 on 2019/12/22.
+//  Copyright © 2019 Ryusei Nomi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomTableViewCell: UITableViewCell
+{
+    let textField = UITextField()
+    let userImage = UIImageView()
+    let time = UITextField()
+}
